06205ca4fdde94f6179881bb6407126fcb7c8a69
Tyler Schnabel <tylerwschnabel@gmail.com>
2022-10-11 11:28:43 -0700

    Finished the game

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import  NCAALogo  from './files/NCAA-Logo.png'
 import Scoreboard from "./components/Scoreboard";
 import { schools } from "./components/Schools";
@@ -9,21 +9,32 @@ import CardArea from "./components/CardArea";
 const App =()=> {
 
   const [cards, setCards] = useState([]);
-  //const [clicked, setClicked] = useState([]);
+  const [clicked, setClicked] = useState([]);
   const [score, setScore] = useState(0);
   const [best, setBest] = useState(0);
  
-  const shuffleCards = () =>{
+  useEffect(()=>{
     const shuffledDeck = schools
     for (let i = shuffledDeck.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [shuffledDeck[i], shuffledDeck[j]] = [shuffledDeck[j], shuffledDeck[i]];
     }
+    checkBest();
     setCards(shuffledDeck);
+  }) 
+
+  const cardClicked = (id) => {
+    if(clicked.includes(id)){
+      reset();
+    } else {
+      rightAnswer();
+      setClicked([...clicked, id]);
+    }
   }
 
-  /* const rightAnswer = () => {
+   const rightAnswer = () => {
       setScore(score + 1);
+      
   }
 
   const reset = () => {
@@ -32,10 +43,10 @@ const App =()=> {
   }
 
   const checkBest = () => {
-      if (score > best){
+      if (score >=best){
           setBest(score);
       }
-  } */
+  }
 
   return (
     <div className="App">
@@ -43,10 +54,13 @@ const App =()=> {
       <header className="App-header">
         <img src={ NCAALogo } alt='NCAA Logo' className="headLogo"/>
         <h1 className="title">College Football Memory Game</h1>
-        <Scoreboard score ={score} best={ best }/>
+        <Scoreboard score ={ score } best={ best }/>
       </header>
       <div className="gameArea">
-        <CardArea shuffleCards={ shuffleCards } cards ={ cards }/>
+        <div className="instructions">
+          <p>Click on a school to make your selection. Make sure to only click on each school once. Try to reach the perfect score of 30!</p>
+        </div>
+        <CardArea cardClicked={ cardClicked } cards ={ cards }/>
         
       </div>
     </div>
--- src/components/CardArea.js
+++ src/components/CardArea.js
@@ -3,12 +3,10 @@ import React from "react";
 
 const CardArea = (props) => {
 
-    const { shuffleCards, cards } = props
-    shuffleCards();
-
+    const { cardClicked, cards } = props
     return <div className="cardArea">
         {cards.map((card) => {
-            return <div className="card" key={card.id} onClick={shuffleCards}>
+            return <div className="card" key={card.id} onClick={()=>cardClicked(card.id)}>
                 <img src={card.img} className="schoolLogo" alt={card.name}/>
                 <p className="schoolName">{card.name}</p>
             </div>
